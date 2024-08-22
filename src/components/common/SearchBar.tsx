f8c6aa2fca530239fbe6f27ff2adeda71342f3cf
Emmanuel B. Giwa <iamtundegiwa@gmail.com>
2024-08-22 17:23:24 +0100

    include search algo

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -1,20 +1,36 @@
 import { SearchIcon } from "../icons";
-import {ChangeEventHandler, useContext, useEffect, useState} from "react";
+import {ChangeEventHandler, useContext, useState} from "react";
 import {EventContext} from "@/context/EventContext";
+import {IEvent} from "@/types/component";
+
 
 export default function SearchBar() {
     const eventCtx = useContext(EventContext)
     const [searchInput, setSearchInput] = useState('')
+    const [unfilteredEvents, setUnfilteredEvents] = useState<IEvent[]>([])
+    const [freshSearch, setFreshSearch] = useState(true)
     
-    const updateSearchInput = (event: ChangeEventHandler<HTMLInputElement>) => setSearchInput(event.target.value)
-
-    useEffect(() => {
-        if (searchInput) searchEvents()
-    }, [searchInput]);
+    // const updateSearchInput = (event: ChangeEventHandler<HTMLInputElement>) => setSearchInput(event.target.value)
     
-    const searchEvents = () => {
-        //
-        console.log('Searching events...')
+    const traverseEvents = (e: ChangeEventHandler<HTMLInputElement>) => {
+        const currentSearchInput = e.target.value;
+        
+        if (searchInput.length === 1 && freshSearch) {
+            setUnfilteredEvents(eventCtx.events)
+            setFreshSearch(false)
+        }
+        
+        if (currentSearchInput.length === 0) {
+            eventCtx.updateEvents(unfilteredEvents)
+            setFreshSearch(true)
+        } else {
+            const filteredEvents =  eventCtx.events
+                    ?.filter(event => event.venue.name.toLowerCase().includes(searchInput.toLowerCase()))
+            
+            if (filteredEvents) eventCtx.updateEvents(filteredEvents)
+        }
+        
+        setSearchInput(currentSearchInput)
     }
     
     return (
@@ -25,7 +41,7 @@ export default function SearchBar() {
                 placeholder={`${searchInput || "Search" }`}
                 className="border-transparent focus:border-transparent outline-none text-black-400 focus:ring-0 w-full"
                 value={searchInput}
-                onChange={updateSearchInput}
+                onChange={traverseEvents}
                 />
         </div>
     )
